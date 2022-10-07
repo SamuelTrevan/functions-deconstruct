3f3363710bd76c1d07a49eaa4dfae576e13a8801
Samuel Trevan <samuel.trevan@gmail.com>
2022-10-07 10:27:32 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,56 @@
+/*
+    Pick somthing you like to do, or something that you can do well that involes many steps
+Playing video games:
+1. Turn on computer
+2. Select profile
+3. Log in to profile
+4. Navigate to Steam
+5. Open Steam
+6. Select Libray
+7. Choose game
+8. Run game
+9. Play game
+
+    your job is to deconstruct your hobby,job, or craft into the tasks required
+        1. Write a function for each task
+        2. in the function body, just console.log() a description of the task
+        3. Invoke the tasks in the correct order
+*/
+
+const turnOnComputer = () => {
+  console.log("Push the powerbutton on the computer");
+};
+const selectProfile = () => {
+  console.log("Select the Profile Samuel");
+};
+const logIntoProfile = () => {
+  console.log("Enter Password");
+};
+const navigateToSteam = () => {
+  console.log("Locate Steam icon on desktop");
+};
+const openSteam = () => {
+  console.log("Double click on Steam icon");
+};
+const selectLibrary = () => {
+  console.log("Select Library in Steam");
+};
+const chooseGame = () => {
+  console.log("Select the thumbnail of the game");
+};
+const runGame = () => {
+  console.log("Select the play button on the game thubnail");
+};
+const playGame = () => {
+  console.log("Play Game");
+};
+
+turnOnComputer();
+selectProfile();
+logIntoProfile();
+navigateToSteam();
+openSteam();
+selectLibrary();
+chooseGame();
+runGame();
+playGame();
